35abf54e0667bf95aa599cc72637e5e1fc17011c
Archit garg <architgarg603@gmail.com>
2021-03-09 16:02:54 +0530

    cli completed

---
+++ mycli.js
@@ -0,0 +1,25 @@
+let helperFile = require("./commands/help.js");
+let viewFile = require("./commands/view");
+let organizeFile = require("./commands/organize");
+
+let input = process.argv.slice(2);
+let command = input[0];
+
+switch (command) {
+    case "view":
+        viewFile.fn(input[1], input[2]);
+        break;
+    case "organize":
+        organizeFile.fn(input[1]);
+        break;
+    case "help":
+        helperFile.fn();
+        break;
+    default:
+        console.log("wrong command type help for all the commands");
+        break;
+}
+
+//view
+//organize
+//help
